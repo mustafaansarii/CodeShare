32bc696c99285d7c73cfe3914c6accff1da2ca07
Mustafa Ansari <mustafaa.edu@gmail.com>
2026-03-01 11:36:26 +0530

    feat: add API proxy configurations for local development and Vercel deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://compiler-api-latest.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   optimizeDeps: {
     include: [
       'monaco-editor/esm/vs/editor/editor.main.js',
